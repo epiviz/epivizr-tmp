8f58e26f7b5051413c9148c3e8a1acbd06334fb6
hcorrada <hcorrada@gmail.com>
2013-03-05 17:33:31 -0500

    reference classes defined

--- pkg/R/AllClasses.r
+++ pkg/R/AllClasses.r
@@ -1,60 +1,69 @@
-
-#' Connection with websockets backend
-#' 
-#' @export
-setClass("EpivizDataConnection", 
-          representation=representation(backend="environment"))
-         
 #' epiviz interactive track manager
 #' 
 #' This class is used to add and delete interactive devices in the epiviz browser. It inherits
 #' from the \link{environment} class, so \link{get} and \link{set} methods exist (but not recommended 
 #' for use). Setters and getters are defined for the pertinent slots. 
 #' 
-#' @section Slots:
+#' @section Fields:
 #' \describe{
 #'  \item{\code{devices}:}{A list of \link{epivizDevice} objects defining currently loaded}
 #'  \item{\code{activeID}:}{ID of currently active device}
-#'  \item{\code{conn}:}{An object of class \link{EpivizDataConnection}}
+#'  \item{\code{server}:}{An environment implementing a websockets server}
+#' }
+#' 
+#' @section Methods:
+#' \describe{
+#'  \item{\code{stop}:}{Stop communiation and close websocket server}
+#'  \item{\code{addDevice}:}{Add a \link{epivizDevice} object to the list of devices}
+#'  \item{\code{delDevice}:}{Remove device from list}
+#'  \item{\code{setActive}:}{Set device as active (for navigation on browser)}
+#'  \item{\code{listDevices}:}{List current devices}
+#'  \item{\code{refresh}:}{Refresh epiviz browser}
+#'  \item{\code{navigate}:}{Navigate to given genomic region in browser}
 #' }
 #' 
 #' @name EpivizDeviceMgr-class
 #' @rdname EpivizDeviceMgr-class
 #' 
 #' @export
-setClass("EpivizDeviceMgr", contains="environment",
-         validity=function(object) {
-           if (!exists("devices", object)) return(FALSE)
-           if (length(object$devices) != object$nDevices) return(FALSE)
-           if (!exists("conn", object)) return(FALSE)
-           if (!is_a(object$conn, "EpivizDataConnection")) return(FALSE)
-           if (!exists("activeID", object)) return(FALSE)
-           if (object$activeID > length(object$nDevices)) return(FALSE)
-           return(TRUE)
-         })
+EpivizDeviceMgr <- setRefClass("EpivizDeviceMgr", 
+                        fields=list(devices="list",
+                                    activeID="character",
+                                    server="environment"),
+                        methods=list(stop=function() {},
+                                     addDevice=function() {},
+                                     delDevice=function() {},
+                                     setActive=function() {},
+                                     listDevices=function() {},
+                                     refresh=function() {},
+                                     navigate=function() {}))
 
 #' epiviz interactive device
 #' 
 #' This class wraps a GenomicRanges object to be plotted in an interactive epiviz device
 #' 
-#' @section Slots:
+#' @section Fields:
 #' \describe{
-#'  \item{\code{id}:}{Id given by the device manager}
-#'  \item{\code{objname}:}{The name of the \link{GRanges-class} object bound to this track}
-#'  \item{\code{type}:}{Tracks display type, currently "regions", "line" or "gene"}
-#'  \item{\code{matcols}:}{vector of column names to use as measurements in non-region devices}
+#'  \item{\code{gr}:}{The the \link{GRanges-class} object bound to this track}
+#'  \item{\code{type}:}{Display type, currently "regions", "line" or "gene"}
+#'  \item{\code{mdCols}:}{vector of elementMetaData column names to use as measurements in plots}
 #'  \item{\code{minValue}:}{minimum value for y axis}
 #'  \item{\code{maxValue}:}{maximum value for y axis}
+#' }
+#' 
+#' @section Methods:
+#' \describe{
+#'  \item{\code{getData}:}{Subset data from the GRanges object within given region}
 #' }
 #' 
 #' @name EpivizDevice-class
 #' @rdname EpivizDevice-class
 #' 
 #' @export
-setClass("EpivizDevice",
-         representation=representation(
-           objname="character",
-           type="character",
-           matcols="character",
-           minValue="numeric",
-           maxValue="numeric"))
+EpivizDevice <- setRefClass("EpivizDevice",
+         fields=list(gr="GenomicRanges",
+            type="character",
+            mdCols="character",
+            minValue="numeric",
+            maxValue="numeric"),
+        methods=list(getData=function() {}))
