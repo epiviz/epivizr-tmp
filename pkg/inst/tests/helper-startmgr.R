01f127611e90bd5cb2f7fe65ed61bfcb16a23cd1
hcorrada <hcorrada@gmail.com>
2013-04-18 03:40:30 -0400

    fetch tests passed

--- pkg/inst/tests/helper-startmgr.R
+++ pkg/inst/tests/helper-startmgr.R
@@ -1,8 +1,8 @@
 localURL="http://localhost/~hcorrada/epiviz/test_socket.php"
-.startMGR=function(openBrowser=FALSE) {
+.startMGR=function(openBrowser=FALSE,...) {
   if (openBrowser) {
-    tryCatch(startEpiviz(localURL=localURL, debug=TRUE, proxy=TRUE, openBrowser=TRUE), interrupt=function(e) invisible())
+    tryCatch(startEpiviz(localURL=localURL, debug=TRUE, proxy=TRUE, openBrowser=TRUE, ...), interrupt=function(e) invisible())
   } else {
-    startEpiviz(localURL=localURL,debug=TRUE,openBrowser=FALSE)
+    startEpiviz(localURL=localURL,debug=TRUE,openBrowser=FALSE, ...)
   }
 }
